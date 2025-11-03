eb67bd8ebb0f73b62811c0908f5e4265bcf3edc3
Alex3236 <me@alex3236.moe>
2025-11-03 23:08:44 +0800

    fix(SearchPage): loading animation

--- components/SearchPage.tsx
+++ components/SearchPage.tsx
@@ -38,6 +38,7 @@ export default function SearchPage({
 }) {
   const router = useRouter();
   const submitButtonRef = React.useRef<HTMLButtonElement>(null);
+  const [isLoading, setIsLoading] = React.useState(false);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -56,6 +57,7 @@ export default function SearchPage({
     }
 
     if (route && route !== currentRoute) {
+      setIsLoading(true);
       submitButtonRef.current?.setAttribute("disabled", "true");
       router.push(route);
     }
@@ -63,6 +65,7 @@ export default function SearchPage({
 
   useEffect(() => {
     if (result && submitButtonRef.current) {
+      setIsLoading(false);
       submitButtonRef.current.disabled = false;
     }
   }, [result])
@@ -124,9 +127,10 @@ export default function SearchPage({
             type="submit"
             ref={submitButtonRef}
             className="bg-blue-600 text-white px-4 py-2 hover:bg-blue-700 focus:ring-2 focus:ring-blue-400 focus:outline-none rounded-full disabled:bg-blue-400 disabled:cursor-not-allowed"
+            disabled={isLoading}
           >
             {
-              submitButtonRef.current?.disabled ?
+              isLoading ?
                 <svg className="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                   <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                   <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
